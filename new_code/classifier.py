183bdfc5f653724308fe28d40102e506c987d12c
elissali <60203041+elissali@users.noreply.github.com>
2020-05-05 00:22:34 -0700

    working up until dloss

--- new_code/classifier.py
+++ new_code/classifier.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 import time
 import torch
 import torch.optim as optim
@@ -16,7 +17,8 @@ config['phase'] = 'train'                                       # or: 'test'
 
 ### DEVICE ###
 config['gpu'] = '0,1'
-config['device'] = torch.device('cuda:'+ config['gpu'])         # or: torch.device('cpu')
+# config['device'] = torch.device('cuda:'+ config['gpu'])         # or: torch.device('cpu')
+config['device'] = torch.device('cpu')
 
 ### MODEL ###
 config['ckpt_timelabel'] = None
@@ -56,12 +58,23 @@ config['continue_train'] = False
 save_config_file(config)
 
 # split data into train, val, test
-train, val, test = get_data()
-trainData = DataLoader(train, batch_size = config['batch_size'], shuffle=True)
-valData = DataLoader(val, batch_size = config['batch_size'], shuffle=True)
-testData = DataLoader(test, batch_size = config['batch_size'])
+if path.exists("train_label_aug.npy"):
+    train_data = np.load("train_aug.npy")
+    val_data = np.load("val_aug.npy")
+    test_data = np.load("test_aug.npy")
+    train_label = np.load("train_label_aug.npy")
+    val_label = np.load("val_label_aug.npy")
+    test_label = np.load("test_label_aug.npy")
+else: 
+    train_data, val_data, test_data, train_label, val_label, test_label = get_data()
 
-print("classifier.py: line 64: done loading data")
+print(train_data.shape)
+print(train_data[0].shape)
+train_data = np.reshape(train_data, (2048, 1, 64, 64, 64))
+# testData = DataLoader(test, batch_size = config['batch_size'])
+# trainLabel = DataLoader(train)
+
+print("classifier.py: line 76: done loading data")
 
 # model
 input_img_size = (config['img_size'][0], config['img_size'][1], config['img_size'][2])
@@ -77,14 +90,14 @@ optimizer = optim.Adam(model.parameters(), lr=config['lr'])
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=10, min_lr=1e-5)   # dynamic change lr according to val_loss
 
 
-def train(model, trainData, valData, config):
+def train(model, train_data, train_label, val_data, val_label, config):
 
     if config['pretrained']:
         model = load_pretrained_model(model, config['device'], ckpt_path=config['pretrained_path'])
 
     iter = 0 # iteration number; cumulative across epochs
     monitor_metric_best = 0
-    iter_per_epoch = len(trainData.loader)
+    iter_per_epoch = len(train_data)
 
     # store final loss from end of each epoch
     losses_total_epochs = []
@@ -93,11 +106,11 @@ def train(model, trainData, valData, config):
 
     learning_rates = []
 
-    print("classifier.py: line 95")
+    print("classifier.py: line 108")
     
     for epoch in range(0, config['epochs']):
 
-        print("classifier.py: line 100, epoch #", epoch)
+        print("classifier.py: line 112, epoch #", epoch)
 
         start_time = time.time()
         model.train()
@@ -111,10 +124,20 @@ def train(model, trainData, valData, config):
         loss_data = 0 # running data loss
         losses_reg = [] # regularization loss history
         loss_reg = 0 # running regularization loss
-        for i, sample in enumerate(trainData.loader, 0):
-            iter += 1
+        
+        for i in range(10):
+            print("classifier.py line 128: ", i)
+            idx_perm = np.random.permutation(int(train_data.shape[0]/2))
+            idx = idx_perm[:int(config['batch_size']/2)]
+            idx = np.concatenate((idx,idx+int(train_data.shape[0]/2)))
 
-            imgs, labels = sample
+            train_imgs = train_data[idx]
+            train_labels = train_label[idx]
+            imgs = torch.from_numpy(train_imgs).to(config['device'], dtype=torch.float)
+            labels = torch.from_numpy(train_labels).to(config['device'], dtype=torch.long)
+
+
+            iter += 1
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -135,7 +158,7 @@ def train(model, trainData, valData, config):
             losses_total.append(dloss+rloss)
 
             # backward pass
-            print("classifier.py: line 138")
+            print("classifier.py: line 157")
             loss.backward()
             if config['clip_grad']:
                 torch.nn.utils.clip_grad_norm_(model.parameters(), config['clip_grad_value'])
@@ -170,7 +193,7 @@ def train(model, trainData, valData, config):
 
         # perform validation for this epoch. Note that the scheduler depends on validation results.
         print('Epoch: [%3d] Validation Results' % (epoch))
-        monitor_metric = evaluate(model, valData, loss_cls_fn, pred_fn, config, info='val')
+        monitor_metric = evaluate(model, val_data, val_label, loss_cls_fn, pred_fn, config, info='val')
         scheduler.step(monitor_metric)
         lr = optimizer.param_groups[0]['lr']
         print('lr: ', lr)
@@ -197,7 +220,7 @@ def test(model, testData, loss_cls_fn, pred_fn, config):
     [model], _ = load_checkpoint_by_key([model], config['ckpt_path'], ['model'], config['device'], config['ckpt_name'])
     evaluate(model, testData, loss_cls_fn, pred_fn, config, info='Test')
 
-def evaluate(model, testData, loss_cls_fn, pred_fn, config, info='Default'):
+def evaluate(model, test, test_label, loss_cls_fn, pred_fn, config, info='Default'):
     model.eval()
     pred_all = []
     label_all = []
@@ -211,9 +234,9 @@ def evaluate(model, testData, loss_cls_fn, pred_fn, config, info='Default'):
     loss_reg = 0  # running regularization loss
     with torch.no_grad():   # else, the memory explode during model(img)
 
-        for iter, sample in enumerate(testData.loader):
-            imgs = sample['image'].to(config['device'], dtype=torch.float)
-            labels = sample['label'].to(config['device'], dtype=torch.long)
+        for i in range(len(test)):
+            imgs = test.to(config['device'], dtype=torch.float)
+            labels = test_label.to(config['device'], dtype=torch.long)
             output = model(imgs)
             pred = pred_fn(output[0])
 
@@ -231,7 +254,7 @@ def evaluate(model, testData, loss_cls_fn, pred_fn, config, info='Default'):
             pred_all.append(pred.detach().cpu().numpy())
             label_all.append(labels.cpu().numpy())
 
-    loss_mean = loss / (2*(iter + 1))
+    loss_mean = loss / (2*(i + 1))
     print(info, loss_mean)
     pred_all = np.concatenate(pred_all, axis=0)
     label_all = np.concatenate(label_all, axis=0)
@@ -250,7 +273,7 @@ def evaluate(model, testData, loss_cls_fn, pred_fn, config, info='Default'):
     return acc
 
 if config['phase'] == 'train':
-    train(model, trainData, valData, config)
+    train(model, train_data, train_label, val_data, val_label, config)
     save_result_figure(config)
 else:
-    test(model, testData, loss_cls_fn, pred_fn, config)
+    test(model, test, test_label, loss_cls_fn, pred_fn, config)
